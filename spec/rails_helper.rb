e66333272d14534b14c80c2c08a36281f5c1e6c4
Tomek Meus <70977982+old-twister@users.noreply.github.com>
2022-11-15 22:02:40 +0100

    confog lines for ShouldaMatchers and Database Cleaner added to rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,4 +17,25 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+  config.include FactoryBot::Syntax::Methods
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.start
+  end
+
+  config.append_after do
+    DatabaseCleaner.clean
+  end
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
